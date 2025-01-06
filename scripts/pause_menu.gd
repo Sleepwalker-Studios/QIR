73129bd5b7abfa724a87b540a6b8cff2039b7026
Lucas Robson <returnnada@gmail.com>
2025-01-05 22:18:13 -0500

    Updated puck + menus

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,10 +1,13 @@
 extends Control
 
 @onready var control = $"../"
+@onready var resume: Button = $MarginContainer/VBoxContainer/Resume
+
+func _ready() -> void:
+	resume.grab_focus()
 
 func _on_resume_pressed() -> void:
 	control.pauseMenu()
-
 
 func _on_quit_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Modes.tscn")
